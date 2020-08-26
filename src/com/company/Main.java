ea6d32425823a7003fe52fc3d7b43e4b4cc0e8b5
DimetriuS92 <Fakestyle92@gmail.com>
2020-08-27 01:43:01 +0300

    monday

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите день недели:");
-        String day = scanner.nextLine();
+        String day = "monday";
         if (day.equals("monday") || day.equals("tuesday") || day.equals("wednesday") || day.equals("thursday") || day.equals("friday")) {
             System.out.println("будний день - работай");
         } else if (day.equals("saturday") || day.equals("sunday") ) {
